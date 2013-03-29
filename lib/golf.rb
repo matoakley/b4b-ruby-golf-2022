07543fe8a5bcd0d39e56f6c6baece68bd5815566
Kerry Buckley <kerryjbuckley@gmail.com>
2013-03-29 11:36:16 +0000

    Nine empty holes

--- lib/golf.rb
+++ lib/golf.rb
@@ -1,6 +1,5 @@
 module Golf
   def self.hole_1
-    puts "hello"
   end
 
   def self.hole_2
@@ -10,5 +9,20 @@ module Golf
   end
 
   def self.hole_4
+  end
+
+  def self.hole_5
+  end
+
+  def self.hole_6
+  end
+
+  def self.hole_7
+  end
+
+  def self.hole_8
+  end
+
+  def self.hole_9
   end
 end
--- spec/golf_spec.rb
+++ spec/golf_spec.rb
@@ -2,26 +2,56 @@ require_relative "../lib/golf"
 
 describe Golf do
   describe ".hole_1" do
-    it "blah" do
+    it "needs a problem to solve" do
+      fail
     end
   end
 
   describe ".hole_2" do
-    it "foo" do
+    it "needs a problem to solve" do
+      fail
     end
   end
 
   describe ".hole_3" do
-    it "foo" do
-    end
-
-    it "blah" do
+    it "needs a problem to solve" do
       fail
     end
   end
 
   describe ".hole_4" do
-    it "foo" do
+    it "needs a problem to solve" do
+      fail
+    end
+  end
+
+  describe ".hole_5" do
+    it "needs a problem to solve" do
+      fail
+    end
+  end
+
+  describe ".hole_6" do
+    it "needs a problem to solve" do
+      fail
+    end
+  end
+
+  describe ".hole_7" do
+    it "needs a problem to solve" do
+      fail
+    end
+  end
+
+  describe ".hole_8" do
+    it "needs a problem to solve" do
+      fail
+    end
+  end
+
+  describe ".hole_9" do
+    it "needs a problem to solve" do
+      fail
     end
   end
 end
